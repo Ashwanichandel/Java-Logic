a48b75d4af392cf7e7cac2b2e80c2bbd140eb0c9
Ashwani chandel <ashwanichandel93964@gmail.com>
2025-08-16 13:28:24 +0530

    rotate  array by one

--- Java_DSA/Array/RemoveDuplicateFromSortedArray.java
+++ Java_DSA/Array/RemoveDuplicateFromSortedArray.java
@@ -1,4 +1,25 @@
 package com.ashwani.connectionjdbc.Java_DSA.Array;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class RemoveDuplicateFromSortedArray {
+    public static void main(String[] args) {
+        int arr[] = {1,1,2,2,3,3,3};
+        System.out.println(removeDuplicate(arr));
+    }
+
+    public static int removeDuplicate(int[] arr) {
+        if(arr.length==0)return -1;
+        int index = 1;
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i]!=arr[i-1]){
+                arr[index]=arr[i];
+                index++;
+            }
+        }
+        return index;
+    }
 }
+
+
