c7d789442121da9e93ce670c3881d797b0dc94a1
Ashwani chandel <ashwanichandel93964@gmail.com>
2025-08-27 15:07:59 +0530

    union of two sorted arrays

--- Java_DSA/Array/Searching/BubbleSort.java
+++ Java_DSA/Array/Searching/BubbleSort.java
@@ -1,4 +1,24 @@
 package com.ashwani.connectionjdbc.Java_DSA.Array.Searching;
 
 public class BubbleSort {
+    public static void main(String[] args) {
+        int arr[] = {1, 5, 3, 7, 2, 4, 1};
+        bubbleSort(arr);
+    }
+
+    public static void bubbleSort(int arr[]) {
+        for (int i = 0; i < arr.length-1; i++) {
+            int temp = 0;
+            for (int j = 0; j < arr.length-1; j++) {
+                if (arr[i] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        for(int i=1;i<arr.length-1;i++){
+            System.out.println(arr[i]);
+        }
+    }
 }
