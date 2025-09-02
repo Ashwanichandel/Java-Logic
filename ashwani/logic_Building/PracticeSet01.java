fde05d631e99a9c9a519b4a50ffe14eb09ed51a5
Ashwani chandel <ashwanichandel93964@gmail.com>
2025-09-02 22:43:14 +0530

    reverse array

--- ashwani/logic_Building/PracticeSet01.java
+++ ashwani/logic_Building/PracticeSet01.java
@@ -1,4 +1,18 @@
 package com.ashwani.connectionjdbc.ashwani.logic_Building;
 
 public class PracticeSet01 {
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 6, 45, 76, 34};
+        System.out.println(max(arr));
+    }
+
+    public static int max(int arr[]) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                 max=arr[i] ;
+            }
+        }
+        return max;
+    }
 }
